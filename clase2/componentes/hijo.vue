d10031d064d34e53ba43011ecbeaad6a4d9d42f3
Mario García Alarcón <mariogarciaalarcon@gmail.com>
2019-09-22 23:53:38 +0200

    clase 2 semifinished

---
+++ clase2/componentes/hijo.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>TITULO IMPRESIONANTE</h1>
+</template>
+
+<script>
+module.exports = {
+    data: function() {
+        return {
+            who: 'world'
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h1 {
+        font-size: 90px;
+        font-style: italic;
+    }
+</style>
