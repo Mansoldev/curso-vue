4e80769897bb4a6cbab3626f6e69a893ab02161d
Mario García Alarcón <mariogarciaalarcon@gmail.com>
2019-10-01 23:37:06 +0200

    vue info dentro

---
+++ vue-info/src/components/index/proyectHoverCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-hover>
+    <template v-slot:default="{ hover }">
+      <v-card
+        class="mx-auto"
+        max-width="500"
+        max-height="500"
+      >
+        <v-img :src="cardInfo.imagen"></v-img>
+
+        <v-fade-transition>
+          <router-link :to="cardInfo.page">
+            <v-overlay
+              v-if="hover"
+              absolute
+              color="#036358"
+            >
+              <v-btn color="#41b883">{{ cardInfo.titulo }}</v-btn>
+            </v-overlay>
+          </router-link>
+        </v-fade-transition>
+      </v-card>
+    </template>
+  </v-hover>
+</template>
+
+<script>
+  export default {
+    props: {
+      cardInfo: Object
+    },
+    data () {
+      return {
+        overlay: false
+      }
+    }
+  }
+</script>
---
+++ vue-info/src/pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home">
+    <v-container>
+      <v-row align="center" justify="center">
+        <v-col cols="6" v-for="cardInfo in cardsInfo" :key="cardInfo.titulo">
+          <hover-card :cardInfo="cardInfo"></hover-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import hoverCard from '@/components/index/proyectHoverCard'
+
+export default {
+  name: 'home',
+  components: {
+    hoverCard
+  },
+  data () {
+    return {
+      cardsInfo: [
+        {
+          imagen: require('@/assets/img/vueFront.jpg'),
+          titulo: 'Proyecto Vue moderno',
+          page: '/modern'
+        },
+        {
+          imagen: require('@/assets/img/vueLegacy.jpg'),
+          titulo: 'Proyecto legacy con vue',
+          page: '/legacy'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .home {
+    height: 100vh;
+    .container {
+      height: 100%;
+      .row {
+        height: 100%;
+      }
+    }
+  }
+</style>
